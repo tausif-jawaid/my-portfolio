574f8e9fa250862434bf63a263357c1516b1afe0
tauisf-jawaid <tausifjawaid6@gmail.com>
2025-07-07 16:18:44 +0530

    Fix vercel routing + base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/', // 👈 MUST match your GitHub repo name exactly
+  base: './', // 👈 change from '/my-portfolio/' to './'
 });
